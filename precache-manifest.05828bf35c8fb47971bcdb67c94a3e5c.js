137f29cdfeeaf84ec7cac49ea35d0e19e0c9e327
Hassnian <hassnianxd@gmail.com>
2019-05-19 16:29:26 +0200

    Updates

---
+++ precache-manifest.05828bf35c8fb47971bcdb67c94a3e5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817d539cc8dfa64c58fe5bf7ded34891",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "82ea34d094daa659408d",
+    "url": "/jokes/static/css/main.3f6a3ca6.chunk.css"
+  },
+  {
+    "revision": "38efa659d3416a8fcdd3",
+    "url": "/jokes/static/js/2.4841977d.chunk.js"
+  },
+  {
+    "revision": "82ea34d094daa659408d",
+    "url": "/jokes/static/js/main.ef2e6f6e.chunk.js"
+  },
+  {
+    "revision": "8811f36da1d3b03f81ba",
+    "url": "/jokes/static/js/runtime~main.4ae1665c.js"
+  }
+]);
